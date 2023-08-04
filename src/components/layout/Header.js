5b3b800a55da4a82c39348d3aaf51a4020d8d9e1
Meet Gol <19bce071@nirmauni.ac.in>
2023-08-04 20:56:58 +0530

    frontned start

---
+++ src/components/layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {ReactNavbar} from "overlay-navbar"
+function Header() {
+  return (
+    <div>
+        {/* Header */}
+    <ReactNavbar />
+
+    </div>
+
+  )
+}
+
+export default Header
